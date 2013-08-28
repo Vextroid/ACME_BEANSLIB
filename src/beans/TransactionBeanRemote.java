08ea01e83b31a59d593440708f6578fbebbcdfda
Vextroid <mark.goldstein17@gmail.com>
2013-08-28 23:06:13 +1000

    trying a deposit method in Transactions

---
+++ src/beans/TransactionBeanRemote.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author Vextroid
+ */
+@Remote
+public interface TransactionBeanRemote {
+ 
+    public void deposit(int C_ID, String accNum, int amount, String description);
+    public void withdrawl(int C_ID, String accNum, int amount, String description);
+    
+    
+}
