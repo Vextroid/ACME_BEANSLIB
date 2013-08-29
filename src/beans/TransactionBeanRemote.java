77e48991e06cadeabe20b5731ad89d8e4ab86738
Vextroid <mark.goldstein17@gmail.com>
2013-08-29 21:11:50 +1000

    PART 1 DONE

--- src/beans/TransactionBeanRemote.java
+++ src/beans/TransactionBeanRemote.java
@@ -4,6 +4,7 @@
  */
 package beans;
 
+import java.util.List;
 import javax.ejb.Remote;
 
 /**
@@ -15,6 +16,6 @@ public interface TransactionBeanRemote {
  
     public void deposit(int C_ID, String accNum, int amount, String description);
     public void withdrawl(int C_ID, String accNum, int amount, String description);
-    
+    public List viewAllTransactions(String accNum);
     
 }
